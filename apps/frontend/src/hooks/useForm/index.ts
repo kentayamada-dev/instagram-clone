ca8d5d983431fb35a4bf6c2ec00032b86dd310af
Kenta Yamada <kentayamada058@gmail.com>
2022-06-18 01:10:46 +0000

    test

--- apps/frontend/src/hooks/useForm/index.ts
+++ apps/frontend/src/hooks/useForm/index.ts
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useLocale } from "../../libs/next_router";
@@ -24,7 +23,6 @@ export const useMyForm: UseMyFormType = ({ isSignup }) => {
     "An unexpected error has occurred. Please wait a few minutes and try again",
     "予期せぬエラーが発生しました。お時間をおいて再度お試しください"
   );
-  const router = useRouter();
   const [login] = useLoginMutation();
   const [signup] = useSignupMutation();
   const [errorMessage, setErrorMessage] = React.useState("");
@@ -56,7 +54,6 @@ export const useMyForm: UseMyFormType = ({ isSignup }) => {
         if (file instanceof Blob) {
           const imageUrl = await getImageUrl({ file });
           await signup({
-            onCompleted: () => router.reload(),
             onError: handleError,
             variables: {
               signupArgs: {
@@ -70,7 +67,6 @@ export const useMyForm: UseMyFormType = ({ isSignup }) => {
         }
       } else {
         await login({
-          onCompleted: () => router.reload(),
           onError: handleError,
           variables: {
             loginArgs: loginProps
