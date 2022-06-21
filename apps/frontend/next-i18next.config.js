50d006d0648a892f3f0beb402cbd5b2ee5ded350
Kenta Yamada <kentayamada058@gmail.com>
2022-06-21 02:14:44 +0000

    update

--- apps/frontend/next-i18next.config.js
+++ apps/frontend/next-i18next.config.js
@@ -5,5 +5,6 @@ module.exports = {
     defaultLocale: "en",
     locales: ["en", "ja"]
   },
+  localePath: path.resolve("./public/locales"),
   reloadOnPrerender: process.env.NODE_ENV === "development" ? true : false
 };
