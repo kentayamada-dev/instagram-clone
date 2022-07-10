d6b94ffea201afc9477d3e1e4ebe8949978713d7
Kenta Yamada <kentayamada058@gmail.com>
2022-07-10 04:58:12 +0000

    Change E2E test on github action

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   videoCompression: 1,
   e2e: {
-    baseUrl: "http://localhost:3002"
+    baseUrl: "https://app.instagram-clone.net"
   }
 });
