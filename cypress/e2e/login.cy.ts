2bbeeef36780ac6b7e61908b581c1284dff873ee
Kenta Yamada <kentayamada058@gmail.com>
2022-07-10 14:55:31 +0000

    update e2e test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,55 +1,30 @@
-import { email, password, errorMessage, invalidEmailMessage, invalidPasswordMessage } from "../fixtures/form.json";
+import { email, password } from "../fixtures/form.json";
 
-describe("Login", () => {
+describe("Login Page", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.url().should("include", "/");
   });
 
-  describe("Success", () => {
-    it("Correct email and password", () => {
-      cy.get('[type="email"]').type(email).should("have.value", email);
-      cy.get('[type="password"]').type(password).should("have.value", password);
-      cy.get("form").submit();
-      cy.contains("p", "Recommend");
-      cy.location("pathname").should("include", "/");
-      cy.getCookie("accessToken").should("exist");
-    });
+  it("Locale", () => {
+    let times = 3;
+    cy.get('[type="button"][aria-label="Toggle Language Mode"]').as("toggleLocaleButton");
+    while (times--) {
+      cy.contains("Email");
+      cy.get("@toggleLocaleButton").contains("A").click();
+      cy.url().should("include", "/ja/");
+      cy.contains("メールアドレス");
+      cy.get("@toggleLocaleButton").contains("あ").click();
+      cy.url().should("include", "/");
+    }
   });
 
-  describe("Invalid", () => {
-    it("Email", () => {
-      const invalidEmail = "invalid@gmail";
-      cy.get('[type="email"]').type(invalidEmail).should("have.value", invalidEmail);
-      cy.get('[type="password"]').type(password).should("have.value", password);
-      cy.get("form").submit();
-      cy.contains("div", invalidEmailMessage);
-    });
-
-    it("Password", () => {
-      const invalidPassword = "invalidPassword";
-      cy.get('[type="email"]').type(email).should("have.value", email);
-      cy.get('[type="password"]').type(invalidPassword).should("have.value", invalidPassword);
-      cy.get("form").submit();
-      cy.contains("div", invalidPasswordMessage);
-    });
-  });
-
-  describe("Fail", () => {
-    afterEach(() => {
-      cy.get("form").submit();
-      cy.contains("div", errorMessage);
-    });
-
-    it("Wrong email", () => {
-      const wrongEmail = email + "wrong";
-      cy.get('[type="email"]').type(wrongEmail).should("have.value", wrongEmail);
-      cy.get('[type="password"]').type(password).should("have.value", password);
-    });
-
-    it("Wrong password", () => {
-      const wrongPassword = password + "wrong";
-      cy.get('[type="email"]').type(email).should("have.value", email);
-      cy.get('[type="password"]').type(wrongPassword).should("have.value", wrongPassword);
-    });
+  it("Login", () => {
+    cy.get('[type="email"]').type(email).should("have.value", email);
+    cy.get('[type="password"]').type(password).should("have.value", password);
+    cy.get("form").submit();
+    cy.contains("p", "Recommend");
+    cy.location("pathname").should("include", "/");
+    cy.getCookie("accessToken").should("exist");
   });
 });
