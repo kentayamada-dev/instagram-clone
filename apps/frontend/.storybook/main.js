49e4e97bfb479a97594b66244ecc6dd47b4012a7
Kenta Yamada <kentayamada058@gmail.com>
2022-07-11 16:02:01 +0000

    update

--- apps/frontend/.storybook/main.js
+++ apps/frontend/.storybook/main.js
@@ -34,50 +34,50 @@ module.exports = {
   managerHead: (head) => {
     return `
       ${head}
-      <meta content="${prefix}/favicon/site-tile-70x70.png" name="msapplication-square70x70logo">
-      <meta content="${prefix}/favicon/site-tile-150x150.png" name="msapplication-square150x150logo">
-      <meta content="${prefix}/favicon/site-tile-310x150.png" name="msapplication-wide310x150logo">
-      <meta content="${prefix}/favicon/site-tile-310x310.png" name="msapplication-square310x310logo">
+      <meta content="/favicon/site-tile-70x70.png" name="msapplication-square70x70logo">
+      <meta content="/favicon/site-tile-150x150.png" name="msapplication-square150x150logo">
+      <meta content="/favicon/site-tile-310x150.png" name="msapplication-wide310x150logo">
+      <meta content="/favicon/site-tile-310x310.png" name="msapplication-square310x310logo">
       <meta content="#0078d7" name="msapplication-TileColor">
-      <link href="${prefix}/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
-      <link href="${prefix}/favicon.ico" rel="icon" type="image/vnd.microsoft.icon">
-      <link href="${prefix}/favicon/apple-touch-icon-57x57.png" rel="apple-touch-icon" sizes="57x57">
-      <link href="${prefix}/favicon/apple-touch-icon-60x60.png" rel="apple-touch-icon" sizes="60x60">
-      <link href="${prefix}/favicon/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
-      <link href="${prefix}/favicon/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76">
-      <link href="${prefix}/favicon/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
-      <link href="${prefix}/favicon/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120">
-      <link href="${prefix}/favicon/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
-      <link href="${prefix}/favicon/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152">
-      <link href="${prefix}/favicon/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180">
-      <link href="${prefix}/favicon/android-chrome-36x36.png" rel="icon" sizes="36x36" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-48x48.png" rel="icon" sizes="48x48" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-72x72.png" rel="icon" sizes="72x72" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-96x96.png" rel="icon" sizes="96x96" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-128x128.png" rel="icon" sizes="128x128" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-144x144.png" rel="icon" sizes="144x144" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-152x152.png" rel="icon" sizes="152x152" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-256x256.png" rel="icon" sizes="256x256" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-384x384.png" rel="icon" sizes="384x384" type="image/png">
-      <link href="${prefix}/favicon/android-chrome-512x512.png" rel="icon" sizes="512x512" type="image/png">
-      <link href="${prefix}/favicon/icon-36x36.png" rel="icon" sizes="36x36" type="image/png">
-      <link href="${prefix}/favicon/icon-48x48.png" rel="icon" sizes="48x48" type="image/png">
-      <link href="${prefix}/favicon/icon-72x72.png" rel="icon" sizes="72x72" type="image/png">
-      <link href="${prefix}/favicon/icon-96x96.png" rel="icon" sizes="96x96" type="image/png">
-      <link href="${prefix}/favicon/icon-128x128.png" rel="icon" sizes="128x128" type="image/png">
-      <link href="${prefix}/favicon/icon-144x144.png" rel="icon" sizes="144x144" type="image/png">
-      <link href="${prefix}/favicon/icon-152x152.png" rel="icon" sizes="152x152" type="image/png">
-      <link href="${prefix}/favicon/icon-160x160.png" rel="icon" sizes="160x160" type="image/png">
-      <link href="${prefix}/favicon/icon-192x192.png" rel="icon" sizes="192x192" type="image/png">
-      <link href="${prefix}/favicon/icon-196x196.png" rel="icon" sizes="196x196" type="image/png">
-      <link href="${prefix}/favicon/icon-256x256.png" rel="icon" sizes="256x256" type="image/png">
-      <link href="${prefix}/favicon/icon-384x384.png" rel="icon" sizes="384x384" type="image/png">
-      <link href="${prefix}/favicon/icon-512x512.png" rel="icon" sizes="512x512" type="image/png">
-      <link href="${prefix}/favicon/icon-16x16.png" rel="icon" sizes="16x16" type="image/png">
-      <link href="${prefix}/favicon/icon-24x24.png" rel="icon" sizes="24x24" type="image/png">
-      <link href="${prefix}/favicon/icon-32x32.png" rel="icon" sizes="32x32" type="image/png">
-      <link href="${prefix}/favicon/manifest.json" rel="manifest">
+      <link href="/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+      <link href="/favicon.ico" rel="icon" type="image/vnd.microsoft.icon">
+      <link href="/favicon/apple-touch-icon-57x57.png" rel="apple-touch-icon" sizes="57x57">
+      <link href="/favicon/apple-touch-icon-60x60.png" rel="apple-touch-icon" sizes="60x60">
+      <link href="/favicon/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+      <link href="/favicon/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76">
+      <link href="/favicon/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+      <link href="/favicon/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120">
+      <link href="/favicon/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+      <link href="/favicon/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152">
+      <link href="/favicon/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180">
+      <link href="/favicon/android-chrome-36x36.png" rel="icon" sizes="36x36" type="image/png">
+      <link href="/favicon/android-chrome-48x48.png" rel="icon" sizes="48x48" type="image/png">
+      <link href="/favicon/android-chrome-72x72.png" rel="icon" sizes="72x72" type="image/png">
+      <link href="/favicon/android-chrome-96x96.png" rel="icon" sizes="96x96" type="image/png">
+      <link href="/favicon/android-chrome-128x128.png" rel="icon" sizes="128x128" type="image/png">
+      <link href="/favicon/android-chrome-144x144.png" rel="icon" sizes="144x144" type="image/png">
+      <link href="/favicon/android-chrome-152x152.png" rel="icon" sizes="152x152" type="image/png">
+      <link href="/favicon/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png">
+      <link href="/favicon/android-chrome-256x256.png" rel="icon" sizes="256x256" type="image/png">
+      <link href="/favicon/android-chrome-384x384.png" rel="icon" sizes="384x384" type="image/png">
+      <link href="/favicon/android-chrome-512x512.png" rel="icon" sizes="512x512" type="image/png">
+      <link href="/favicon/icon-36x36.png" rel="icon" sizes="36x36" type="image/png">
+      <link href="/favicon/icon-48x48.png" rel="icon" sizes="48x48" type="image/png">
+      <link href="/favicon/icon-72x72.png" rel="icon" sizes="72x72" type="image/png">
+      <link href="/favicon/icon-96x96.png" rel="icon" sizes="96x96" type="image/png">
+      <link href="/favicon/icon-128x128.png" rel="icon" sizes="128x128" type="image/png">
+      <link href="/favicon/icon-144x144.png" rel="icon" sizes="144x144" type="image/png">
+      <link href="/favicon/icon-152x152.png" rel="icon" sizes="152x152" type="image/png">
+      <link href="/favicon/icon-160x160.png" rel="icon" sizes="160x160" type="image/png">
+      <link href="/favicon/icon-192x192.png" rel="icon" sizes="192x192" type="image/png">
+      <link href="/favicon/icon-196x196.png" rel="icon" sizes="196x196" type="image/png">
+      <link href="/favicon/icon-256x256.png" rel="icon" sizes="256x256" type="image/png">
+      <link href="/favicon/icon-384x384.png" rel="icon" sizes="384x384" type="image/png">
+      <link href="/favicon/icon-512x512.png" rel="icon" sizes="512x512" type="image/png">
+      <link href="/favicon/icon-16x16.png" rel="icon" sizes="16x16" type="image/png">
+      <link href="/favicon/icon-24x24.png" rel="icon" sizes="24x24" type="image/png">
+      <link href="/favicon/icon-32x32.png" rel="icon" sizes="32x32" type="image/png">
+      <link href="/favicon/manifest.json" rel="manifest">
       <meta content="Instagram Clone Storybook" name="title">
       <meta content="This is an Instagram Clone Storybook." name="description">
       <meta
@@ -99,9 +99,6 @@ module.exports = {
       <meta content="Instagram Clone Storybook" property="twitter:title">
       <meta content="This is an Instagram Clone Storybook." property="twitter:description">
       <meta content="https://app.instagram-clone.net/static/instagram/image.jpg" property="twitter:image">
-      <script>
-        window['PREVIEW_URL'] = '${prefix}/iframe.html';
-      </script>
     `;
   },
   webpackFinal: (config) => {
@@ -109,13 +106,6 @@ module.exports = {
       ...config.resolve.alias,
       "next-i18next": "react-i18next"
     };
-    return merge(config, {
-      output: {
-        publicPath: `${prefix}/`
-      }
-    });
-  },
-  managerWebpack: async (config) => {
     return merge(config, {
       output: {
         publicPath: `${prefix}/`
