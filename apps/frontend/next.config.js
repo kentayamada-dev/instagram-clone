fd323379d54223cfe6cac630ec03a9c4d4a4fb84
Kenta Yamada <kentayamada058@gmail.com>
2022-07-12 14:38:17 +0000

    add nextjs bundle analyzer

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,5 +1,8 @@
 const { i18n } = require("./next-i18next.config");
 
+const withBundleAnalyzer =
+  process.env.ANALYZE === "true" ? require("@next/bundle-analyzer")({ enabled: true }) : (config) => config;
+
 const nextConfig = {
   i18n,
   distDir: "dist",
@@ -20,4 +23,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
