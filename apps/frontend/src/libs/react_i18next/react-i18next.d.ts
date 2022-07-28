59b9ef3d7880b58d653217e4d703791321c137aa
Kenta Yamada <kentayamada058@gmail.com>
2022-07-28 01:45:45 +0000

    update

--- apps/frontend/src/libs/react_i18next/react-i18next.d.ts
+++ apps/frontend/src/libs/react_i18next/react-i18next.d.ts
@@ -1,8 +1,8 @@
 import "react-i18next";
-import type common from "../public/locales/en/common.json";
-import type footer from "../public/locales/en/footer.json";
-import type form from "../public/locales/en/form.json";
-import type notFound from "../public/locales/en/notFound.json";
+import type common from "../../../public/locales/en/common.json";
+import type footer from "../../../public/locales/en/footer.json";
+import type form from "../../../public/locales/en/form.json";
+import type notFound from "../../../public/locales/en/notFound.json";
 
 declare module "react-i18next" {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions, @typescript-eslint/naming-convention
