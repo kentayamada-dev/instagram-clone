31665226d01751a4d94e5eb39abf1e602361bba4
Kenta Yamada <kentayamada058@gmail.com>
2022-08-30 15:46:27 +0000

    test

--- apps/gh-pages/astro.config.mjs
+++ apps/gh-pages/astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://kentayamada-dev.github.io",
+  base: "/instagram-clone",
   integrations: [tailwind()]
 });
