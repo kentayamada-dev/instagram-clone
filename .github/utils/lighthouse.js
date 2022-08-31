cd4e025f2abcd9b8a2c3f8330cb862caf67f6d15
Kenta Yamada <kentayamada058@gmail.com>
2022-08-31 03:39:35 +0000

    update

--- .github/utils/lighthouse.js
+++ .github/utils/lighthouse.js
@@ -1,4 +1,6 @@
 import { convertFileContent } from "./common.js";
+import { renameSync } from "fs";
+import { basename } from "path";
 
 const getContent = (val) => (val < 0 ? `(🟢 +${Math.abs(val)})` : val > 0 ? `(🔴 -${Math.abs(val)})` : "");
 const diff = (prev, current) => Number(prev) - Number(current);
@@ -6,10 +8,18 @@ const diff = (prev, current) => Number(prev) - Number(current);
 export const lighthouse = async (core) => {
   const outputObj = convertFileContent("./results.json");
   const prevOutputObj = convertFileContent("lighthouse/results.json");
-  const stringifiedOutputObj = JSON.stringify(outputObj, null, 2);
   const tds = outputObj
     .map((output) => {
-      const url = new URL(output.url);
+      const urlString = output.url;
+      const url = new URL(urlString);
+      const pathname = url.pathname;
+      const emulatedFormFactor = output.emulatedFormFactor;
+      const modifiedPathname = pathname.replace(/\//g, "");
+      const fileName = modifiedPathname === "" ? "root" : modifiedPathname;
+      const filePath = basename(output.localReport);
+      const renamedFilePath = `lighthouseOutput/${fileName}_${emulatedFormFactor}.html`;
+      renameSync(`lighthouseOutput/${filePath}`, renamedFilePath);
+      output.localReport = renamedFilePath;
       const outputScores = output.scores;
       let prevOutputScores = {
         performance: 0,
@@ -20,7 +30,7 @@ export const lighthouse = async (core) => {
       };
       if (prevOutputObj) {
         const foundPrevOutputObj = prevOutputObj.find(
-          (x) => x.url === output.url && x.emulatedFormFactor === output.emulatedFormFactor
+          (x) => x.url === urlString && x.emulatedFormFactor === emulatedFormFactor
         ).scores;
         if (foundPrevOutputObj) {
           prevOutputScores = foundPrevOutputObj;
@@ -31,7 +41,7 @@ export const lighthouse = async (core) => {
       const bestPracticesDiff = diff(prevOutputScores.bestPractices, outputScores.bestPractices);
       const seoDiff = diff(prevOutputScores.seo, outputScores.seo);
       const pwaDiff = diff(prevOutputScores.progressiveWebApp, outputScores.progressiveWebApp);
-      return `\n| <a href=${output.url}>${url.pathname}</a> | ${output.emulatedFormFactor} | ${
+      return `\n| <a href=${urlString}>${pathname}</a> | ${emulatedFormFactor} | ${
         outputScores.performance
       } ${getContent(performanceDiff)} | ${outputScores.accessibility} ${getContent(accessibilityDiff)} | ${
         outputScores.bestPractices
@@ -40,6 +50,7 @@ export const lighthouse = async (core) => {
       } ${getContent(pwaDiff)} |`;
     })
     .join("");
+  const stringifiedOutputObj = JSON.stringify(outputObj, null, 2);
   const result = `## Lighthouse Results\n\nPath | Device | Performance | Accessibility | Best Practices | SEO | PWA |\n|---|---|---|---|---|---|---|${tds}\n<details>\n\n\`\`\`json\n${stringifiedOutputObj}\n\`\`\`\n\n</details>`;
   await core.summary.addRaw(result).write();
 };
