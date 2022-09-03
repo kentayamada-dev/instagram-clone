95bab0d79455b06d91164f856a1f1f7a9510db8b
Kenta Yamada <kentayamada058@gmail.com>
2022-09-03 07:17:45 +0000

    test cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true
+  },
   videoCompression: 1,
   e2e: {
     baseUrl: "https://app.instagram-clone.net/",
